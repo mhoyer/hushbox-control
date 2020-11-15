447d10eb0ea865537318201deb4cc676dbd2a3ab
Marcel Hoyer <mhoyer@pixelplastic.de>
2020-11-15 19:31:01 +0100

    add support for fan on/off switch

--- src/linear_fan_control.py
+++ src/linear_fan_control.py
@@ -2,15 +2,17 @@ from machine import PWM, Pin
 
 class LinearFanControl:
     def __init__(self, pwm_pin, min_temp=20, max_temp=35, freq=21000):
+        self.pwm_pin = pwm_pin
         pin = Pin(pwm_pin)
         self.pwm = PWM(pin, freq=freq)
         self.min_temp = min_temp
         self.max_temp = max_temp
         self.temp_range = max_temp - min_temp
+        print("New LFC for GPIO {} (t_min={}, t_max={}, freq={})".format(pwm_pin, min_temp, max_temp, freq))
 
     def update_pwm_for_temp(self, temp):
         temp_factor = (temp - self.min_temp) / self.temp_range
-        # print(temp_factor, temp, self.min_temp)
+        print("  update PWM for {}: t_factor={}, temp={}, t_min={}".format(self.pwm_pin, temp_factor, temp, self.min_temp))
         duty = max(0, min(1023, temp_factor * 1024))
 
         self.pwm.duty(int(duty))
--- src/main.py
+++ src/main.py
@@ -3,24 +3,38 @@ from linear_fan_control import LinearFanControl
 from temp_reader import TempReader
 from rpm_reader import RPMReader
 from wifi import connect_wifi
+from machine import Pin
 
 connect_wifi()
 
-lfcIn = LinearFanControl(pwm_pin=22, min_temp=25, max_temp=38)
-lfcOut = LinearFanControl(pwm_pin=27, min_temp=25, max_temp=35)
+lfc_in = LinearFanControl(pwm_pin=22, min_temp=23, max_temp=30)
+lfc_out = LinearFanControl(pwm_pin=27, min_temp=23, max_temp=30)
 
-rpmIn = RPMReader(rpm_pin=23)
-rpmOut = RPMReader(rpm_pin=26)
+rpm_in = RPMReader(rpm_pin=23)
+rpm_out = RPMReader(rpm_pin=26)
 
 temp_reader = TempReader(pin=18)
+fan_switch = Pin(32, Pin.OUT)
 
 while True:
     temps = temp_reader.read_temps()
-    temp = list(temps)[0]
-    lfcIn.update_pwm_for_temp(temp)
-    lfcOut.update_pwm_for_temp(temp)
-    print("Temp={:<7}°C => IN [PWM={:<4} RPM={:<5}] OUT [PWM={:<4} RPM={:<5}]".format(
-        temp,
-        lfcIn.pwm.duty(), rpmIn.rpm,
-        lfcOut.pwm.duty(), rpmOut.rpm))
+    temp = 100
+    try:
+        temp = list(temps)[0]
+    except Exception:
+        pass
+
+    if temp < lfc_out.min_temp:
+        fan_switch.off()
+        print("Temp={:<7}C".format(temp))
+    else:
+        fan_switch.on()
+        lfc_in.update_pwm_for_temp(temp)
+        lfc_out.update_pwm_for_temp(temp)
+
+        print("Temp={:<7}C => IN [PWM={:<4} RPM={:<5}] OUT [PWM={:<4} RPM={:<5}]".format(
+            temp,
+            lfc_in.pwm.duty(), rpm_in.rpm,
+            lfc_out.pwm.duty(), rpm_out.rpm))
+
     sleep(3)
