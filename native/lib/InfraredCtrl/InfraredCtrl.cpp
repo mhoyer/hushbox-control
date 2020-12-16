3c8fb41006c2f6d54dc8251940c9ec41df107b57
Marcel Hoyer <mhoyer@pixelplastic.de>
2020-12-16 21:29:03 +0100

    add auto generated lib/README of native PIO project

--- native/lib/InfraredCtrl/InfraredCtrl.cpp
+++ native/lib/InfraredCtrl/InfraredCtrl.cpp
@@ -28,3 +28,21 @@ uint64_t ir_decode()
   return results.value;
 }
 
+
+  // if (results.value == 0xCA15E)
+  // {
+  //   // ir_send_in.sendNEC(IR_YAMAHA_OFF);
+  //   delay(400);
+  //   ir_send_out.sendNEC(IR_YAMAHA_OFF);
+  // }
+  // else if(results.value == 0xCF00F)
+  // {
+  //   Serial.println("MENU");
+  //   // ir_send_in.sendNEC(IR_YAMAHA_ON);
+  //   delay(400);
+  //   ir_send_out.sendNEC(IR_YAMAHA_ON);
+  // }
+  // else
+  // {
+  //   Serial.println(resultToHumanReadableBasic(&results));
+  // }
